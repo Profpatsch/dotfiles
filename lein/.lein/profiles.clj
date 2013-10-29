78da44d12b91dd0820bff3daeb3bd965cc46f54d
Profpatsch <mail@profpatsch.de>
2013-10-29 14:07:17 +0100

    Leiningen settings.

---
+++ lein/.lein/profiles.clj
@@ -0,0 +1,9 @@
+{:user {:plugins [[lein-droid "0.1.0"]
+                  [lein-midje "3.1.1"]
+                  [lein-search "0.3.4"]
+                  [lein-ancient "0.4.4"]
+                  [lein-kibit "0.0.8"]
+                  [lein-cljsbuild "0.3.2"]]
+                  
+
+        :android {:sdk-path "/opt/android-sdk/"}}}
