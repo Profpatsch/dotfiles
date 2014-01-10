b787e4599460de8a9a957791a9fced5fb8655b92
Profpatsch <mail@profpatsch.de>
2014-01-10 14:39:50 +0100

    lein: Some plugin changes

--- lein/.lein/profiles.clj
+++ lein/.lein/profiles.clj
@@ -1,9 +1,11 @@
-{:user {:plugins [[lein-droid "0.1.0"]
-                  [lein-midje "3.1.1"]
-                  [lein-search "0.3.4"]
+{:user {:plugins [;; [lein-droid "0.1.0"]
+                  ;; [lein-midje "3.1.1"]
+                  ;; [lein-search "0.3.4"]
                   [lein-ancient "0.4.4"]
                   [lein-kibit "0.0.8"]
-                  [lein-cljsbuild "0.3.2"]]
+                  [lein-cljsbuild "0.3.2"]
+                  [lein-ring "0.8.8"]
+                  [quickie "0.2.4"]]
                   
 
         :android {:sdk-path "/opt/android-sdk/"}}}
