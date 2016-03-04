1d327f9ea1139735ae877f044f48b22dd8a89763
Profpatsch <mail@profpatsch.de>
2016-03-04 17:05:24 +0100

    current-balance script

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -2,3 +2,15 @@ set fish_color_command "purple"
 set fish_color_autosuggestion "yellow"
 
 set -e fish_greeting
+
+set -x PATH ~/scripts $PATH
+
+# colored man
+
+set -xU LESS_TERMCAP_mb (printf "\e[01;31m")      # begin blinking
+set -xU LESS_TERMCAP_md (printf "\e[01;31m")      # begin bold
+set -xU LESS_TERMCAP_me (printf "\e[0m")          # end mode
+set -xU LESS_TERMCAP_se (printf "\e[0m")          # end standout-mode
+set -xU LESS_TERMCAP_so (printf "\e[01;44;33m")   # begin standout-mode - info box
+set -xU LESS_TERMCAP_ue (printf "\e[0m")          # end underline
+set -xU LESS_TERMCAP_us (printf "\e[01;32m")      # begin underline
