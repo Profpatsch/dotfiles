d045bd84f51b27a25c4a3b3af8b03872b7db854a
Profpatsch <mail@profpatsch.de>
2018-06-08 21:44:56 +0200

    xmonad config: add window movement for keyboard only

--- xmonad/.xmonad/xmonad.hs
+++ xmonad/.xmonad/xmonad.hs
@@ -29,7 +29,8 @@ myConfig = conf { modMask = mod
                 , workspaces = workspaceNames
                 }
            `additionalKeysP` (
-             [ ("M-e", sendMessage $ Toggle NBFULL)
+             [ -- fullscreen
+               ("M-e", sendMessage $ Toggle NBFULL)
                  -- i3-like keybindings, because I’m spoiled
              , ("M-S-x", kill)
                  -- exchange M-Ret and M-S-Ret
@@ -39,33 +40,53 @@ myConfig = conf { modMask = mod
              -- toogle toolbar(s)
              , ("M-b", sendMessage ToggleStruts)
              -- open simple exec dmenu
-             -- TODO integrate with nixos-config
-             -- , ("M-u", spawn "dmenu_run")
-             -- open password dmenu
-             -- , ("M-p", spawn "passmenu --type")
              ]
              ++
+             -- something something workspaces
              [ (otherModMasks ++ "M-" ++ [key], action tag)
                | (tag, key)  <- zip workspaceNames "123456789"
                , (otherModMasks, action) <- [ ("", windows . W.greedyView)
                                             , ("S-", windows . W.shift)]
              ]
+             ++
+             -- mod-{w,e,r} %! Switch to physical/Xinerama screens 1, 2, or 3
+             -- mod-shift-{w,e,r} %! Move client to screen 1, 2, or 3
+             [ ("M-v", focusToScreen 0)
+             , ("M-l", focusToScreen 1)
+             , ("M-c", focusToScreen 2)
+             , ("M-S-v", windowToScreen 0)
+             , ("M-S-l", windowToScreen 1)
+             , ("M-S-c", windowToScreen 2)
+             ]
+             -- ((m .|. modMask, key), screenWorkspace sc >>= flip whenJust (windows . f))
+             --   | (key, sc) <- zip [xK_w, xK_e, xK_r] [0..]
+             --    , (f, m) <- [(W.view, 0), (W.shift, shiftMask)]]
            )
+           `additionalKeys`
+           -- arrow keys should move as well (hjkl blindness)
+           [ ((mod, xK_Up), windows W.focusUp)
+           , ((mod, xK_Down), windows W.focusDown) ]
            `removeKeysP`
              [
                -- previous kill command
                "M-S-c"
                -- It is way to easy to kill everything by default
              , "M-S-q"
+               -- no idea, I want to use it for Mozc
+             , "M-n"
              ]
   where
-    conf = defaultConfig
+    conf = def
     workspaceNames = workspaces conf
     mod = mod4Mask
     -- TODO: meh
     term :: Mode -> String
     term Normal = "lilyterm"
     term Presentation = "lilyterm -u ~/.config/lilyterm/pres.conf"
+
+    toScreen with number = screenWorkspace 0 >>= \ws -> whenJust ws (windows . with)
+    focusToScreen = toScreen W.view
+    windowToScreen = toScreen W.shift
 
 -- copied from Xmonad.Config
 layout = avoidStruts $ toggleFullscreen $ tiled ||| Mirror tiled
