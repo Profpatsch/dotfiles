16d26621573a0d43a8d5c805f88df85b57a9f8ea
Profpatsch <mail@profpatsch.de>
2020-08-16 09:38:10 +0200

    fish,scripts: move mkPlaylist to scripts

--- fish/.config/fish/functions/mkPlaylist.fish
+++ fish/.config/fish/functions/mkPlaylist.fish
@@ -1,18 +1,4 @@
-# Defined in /tmp/fish.6dQrtG/mkPlaylist.fish @ line 2
+# Defined in /tmp/fish.6bRKOO/mkPlaylist.fish @ line 2
 function mkPlaylist
-	set FILE (mktemp --suffix=".m3u")
-  for dir in $argv
-    find "$dir" \
-      -type f \
-      -and \
-      \( -not \
-         \( -iname "*.cue" \
-        -or -iname "*.log" \
-        -or -iname "*.png" \
-        -or -iname "*.jpg" \
-        -or -iname "*.mp4" \
-        -or -iname "*.mkv" \) \) \
-      >> "$FILE"
-  end
-	echo "$FILE"
+  ~/scripts/mkPlaylist
 end
